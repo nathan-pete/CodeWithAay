9b78c04287f36f5edb6ee80f138cd7e676b0ca6a
Nathan P. Pais D'costa <71150111+nathan-pete@users.noreply.github.com>
2023-01-21 18:16:23 +0100

    File Scripts in Python

---
+++ Files.py
@@ -0,0 +1,8 @@
+# Setting a variable to the path of and opening a file
+File_Path = open('./fileTest.txt', 'a')
+
+# Writing to the file
+File_Path.write('This is a test')
+
+# Closing the file
+File_Path.close()
